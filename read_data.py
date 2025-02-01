87ae4a04632be716645350e12cfccb95fbda4fad
Manuel <manuelv0703@gmail.com>
2025-01-31 20:23:22 -0600

    Added a function to read data

--- read_data.py
+++ read_data.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import numpy as np
+import pandas as pd
+
+def read_data():
+    data = pd.read_csv('training.csv')
+    return data
+
+def main():
+    print(read_data())
+
+if __name__ == '__main__':
+    main()
