980a353458a72f4c3004e91a2655d92ce7319ba0
LemurTech <joseconde101jc@gmail.com>
2025-02-01 03:49:29 -0600

    imported seaborn and updated code

--- cleaning_data.py
+++ cleaning_data.py
@@ -10,7 +10,7 @@ def replace_na(df):
     df = df.replace('Unknown', pd.NA)
 
     msno.bar(df)
-
+    msno.heatmap(df)
     fuel_type_map= {
         'Gasoline': 0,
         'Diesel': 1,
@@ -20,11 +20,33 @@ def replace_na(df):
     }
     df['Fuel_Type'] = df['Fuel_Type'].map(fuel_type_map)
     df = df.drop('Region', axis=1)
+
     df.drop(columns=['Number_of_Vehicles_Registered_at_the_Same_Address'], errors='ignore', inplace=True)
 
+    missing = df['Fuel_Type'].isnull().sum()
+    missing_row = df[df['Fuel_Type'].isnull()]
+
+    df = df.dropna(subset=['Fuel_Type'])
+
+    missing = df['Fuel_Type'].isnull().sum()
+
+    print(missing_row)
+    missing_row = df[df['Fuel_Type'].isnull()]
+
+    print(f'Number of Missing fuel type: ', missing)
     #modify
     #df['GVWR_Class'] = np.where(df['Vehicle_Category']=='p',)
     return df
+def replace_GVWR_Class(df):
+    plt.figure(figsize=(8, 5))
+
+    df.hist(figsize=(15,5), bins = 10, edgecolor='black')
+    plt.suptitle('Histogram of all columns', fontsize=15)
+    plt.show()
+
+
+    df[['Model_Year', 'Fuel_Type']].hist(figsize=(10, 5), bins=15, edgecolor='black')
+    plt.show()
 
 def rename_df(df):
     col = {'Vehicle Category': 'vehicle_category', 'GVWR Class': 'GVWR_Class', 'Fuel Type':  'Fuel_Type', 
@@ -39,8 +61,10 @@ if __name__ == '__main__':
     copy = copy_data('training.csv')
     copy = rename_df(copy)
     copy = replace_na(copy)
-
-    copy.info()
+    plot_data(copy)
+    copy = replace_GVWR_Class(copy)
+    #copy.to_csv('changes.csv')
+    
 
 
 
--- read_data.py
+++ read_data.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import missingno as msno
+import seaborn as sns
+
+
 def read_data(file_name):
     data = pd.read_csv(file_name)
     return data
 
 def plot_data(df):
     df.info()
-    #df.plot(kind = 'scatter', x ='Date', y='Vehicle Population')
+    sns.pairplot(df)
+    plt.show()
 def main():
     print(read_data('training.csv'))
     plot_data(read_data('training.csv'))
