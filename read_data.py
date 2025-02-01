e8eb5cffa9d7af82543c36a3afc915024ac1dfda
Manuel <manuelv0703@gmail.com>
2025-01-31 20:34:51 -0600

    Plotted the data

--- read_data.py
+++ read_data.py
@@ -1,12 +1,18 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def read_data():
-    data = pd.read_csv('training.csv')
+def read_data(file_name):
+    data = pd.read_csv(file_name)
     return data
 
+def plot_data(data):
+    data.plot()
+
 def main():
-    print(read_data())
+    print(read_data('training.csv'))
+    plot_data(read_data('training.csv'))
+    plt.show()
 
 if __name__ == '__main__':
     main()
