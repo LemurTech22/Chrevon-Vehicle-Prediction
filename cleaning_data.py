4d930812dc4cddb2f5366d1ee575636cf8f919cd
LemurTech <joseconde101jc@gmail.com>
2025-01-31 21:33:41 -0600

    added clean data class

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,2 +1,13 @@
 from read_data import *
 
+def copy_data(fileName):
+    copy_df = pd.read_csv(fileName)
+    return copy_df
+
+def replace_NA(df):
+    df = df.replace('Not Applicable', pd.NA)
+    return df.info()
+
+if __name__ == '__main__':
+    copy=copy_data('training.csv')
+    print(replace_NA(copy))
