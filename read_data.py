b838193d0685bca1dda5652bef00f5c288a08c96
Jose Conde <145078308+LemurTech22@users.noreply.github.com>
2025-07-29 01:19:03 -0500

    fixed some plots

--- read_data.py
+++ read_data.py
@@ -1,22 +1,27 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import missingno as msno
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 
-
-def read_data(file_name):
-    data = pd.read_csv(file_name)
-    return data
 
 def plot_data(df):
-    #df.info()
+    df.info()
     sns.pairplot(df)
+    plt.title('Pairplot of Features')
     plt.show()
+    
+def visualize_histogram(df):
+    df.hist(figsize=(10, 10))
+    plt.title('Histogram of Numerical Features')
+    plt.tight_layout()
+
 def main():
-    print(read_data('training.csv'))
-    plot_data(read_data('training.csv'))
+    data = pd.read_csv('training.csv')
+    plot_data(data)
+    visualize_histogram(data)
     plt.show()
 
 if __name__ == '__main__':
     main()
+
